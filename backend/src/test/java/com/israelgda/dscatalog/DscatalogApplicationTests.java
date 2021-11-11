e31cd4dac3d87cd5d4da3de97855f6b28bea666b
Israel Gadelha <gdelha.israel@gmail.com>
2021-11-11 00:20:53 -0300

    Feat: Commit inicial de criação do projeto

---
+++ backend/src/test/java/com/israelgda/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.israelgda.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
