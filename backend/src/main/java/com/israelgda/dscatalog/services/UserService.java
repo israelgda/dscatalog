8f6e5af30766176e538a3369e1205ba586054582
Israel Gadelha <gdelha.israel@gmail.com>
2021-12-04 16:32:36 -0300

    Feat: Implementado o OAuth2 AutorizationServer

--- backend/src/main/java/com/israelgda/dscatalog/services/UserService.java
+++ backend/src/main/java/com/israelgda/dscatalog/services/UserService.java
@@ -4,10 +4,15 @@ import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,8 +30,10 @@ import com.israelgda.dscatalog.services.exceptions.DataBaseException;
 import com.israelgda.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService{
 
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+	
 	private final BCryptPasswordEncoder passwordEncoder;
 	
 	private final UserRepository repository;
@@ -112,6 +119,17 @@ public class UserService {
 		}
 		
 		return newUser;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = repository.findByEmail(username);
+		if (user == null) {
+			logger.error("User not found: " + username);
+			throw new UsernameNotFoundException("Email not found!");
+		}
+		logger.info("User found: " + username);
+		return user;
 	}
 
 }
